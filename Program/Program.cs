d1ad6b157bd3844b2d86905bc009fc887b32b88c
irina <irisha.201@mail.com>
2023-10-03 20:51:07 +0300

    cSharp program have been added

---
+++ Program/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+*/
+
+string[] EnterArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+string[] ChooseNewArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    } 
+    string[] newArray = new string[count];
+    
+    int number = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[number] = array[i];
+            number++;
+        }
+    } 
+    return newArray;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+string[] array = EnterArray(n);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Конечный массив: [{string.Join(", ", ChooseNewArray(array))}]");
